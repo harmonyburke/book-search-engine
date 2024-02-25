56e0293c6c2e99e8a5b5708e3487beb058830571
Harmony Burke <theharmonyburke@gmail.com>
2024-02-24 18:07:41 -0600

    updated all query and mutations is resolvers-query all users and all book, mutations to add new user, login, save books to user profile, and remove saved books

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,22 @@
 const { Book, User } = require('../models');
+const { countDocuments } = require('../models/User');
 const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    // find all users
     User: async () => {
       return User.find({});
     },
+
+    // find all Books
+    Book: async () => {
+      return Book.find({});
+    }
   },
+  
   Mutation: {
+
     // create new user
     addUser: async (parent, args) => {
       const newUser = await User.create(args);
@@ -30,6 +39,20 @@ const resolvers = {
       const token = signToken(profile);
       return { token, profile };
     },
+    saveBook: async(parent, args) => {
+      const userBooks= await User.findOneAndUpdate(
+        {_id: User._id},
+        { $addToSet: {savedBooks: body}},
+        { new: true, runValidators: true}
+      );
+      return userBooks;
+    },
+    // remove Book
+    removeBook: async(parent, args, context) => {
+      if (context.books) {
+        return Book.findOneAndDelete({ bookId:context.book.bookId });
+      }
+    }
   },
 };
 
