cba5bb991d2fd68e2cc103fa735992142bda7e93
Harmony Burke <theharmonyburke@gmail.com>
2024-02-26 19:26:15 -0600

    added queries and mutations to front end

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,57 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password, String!) {
+    loginUser(emai: $email, password: $password) {
+     token
+     user{
+      _id
+      username
+      email
+      bookCount
+     }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, email: String!, password:String!) {
+    addUser(username: $username, email: $email, password $password) {
+     token
+     user{
+      _id
+      username
+      email
+      bookCount
+     }
+    }
+  }
+`;
+ export const REMOVE_BOOK = gql`
+ mutation removeBook($userId: ID~, $bookID:String~){
+  removeBook(userId: $userId, bookId: $bookId){
+    _id
+    username
+    email
+    bookCount
+
+  }
+ }`;
+
+ export const SAVE_BOO= gql`
+ mutation saveBook($userId: ID!, $book: BookInput!){
+  saveBook(userId: $userId, book: $book){
+    _id
+    username
+    email
+    bookCount
+    savedBooks{
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+ }`;
